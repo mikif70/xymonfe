4973a1755e6d24b7c22d9e3d523a108e2eab0c59
miki <mf@fed-e.com>
2015-01-24 16:20:35 +0100

    .

--- client/view/xymonfe.js
+++ client/view/xymonfe.js
@@ -1,33 +1,17 @@
 Template.xymonfe.helpers({
-  hostname: function(key) {
-    retval = key.split("::");
-    return retval[2]
-  },
-
-  service: function(key) {
-    var retval = key.split("::");
-    return retval[1];
-  },
-
-  info: function(key) {
-    var retval = key.split("::");
-    console.log(retval[3]);
-    return retval[3]
-  },
-
-  val: function(key, value) {
-    var retval = value.split("::");
-    console.log(retval[0], retval[1]);
-    $("#label_"+key).css("background-color", retval[0]);
-    return retval[1];
-  },
 
   xymon: function() {
-    var retval = []
     var dbq =  xymon.matching('xymon::*').fetch();
     var i = 0;
     dbq.forEach(function(d) {
       d["index"] = i;
+      sp = d["key"].split("::");
+      d["service"] = sp[1];
+      d["host"] = sp[2];
+      d["test"] = sp[3];
+      vl = d["value"].split("::");
+      d["color"] = vl[0];
+      d["val"] = vl[1];
       console.log(d);
       i++;
     });
