1c39cc7428033721474386ffedf2e9c2df7fb5e7
miki <mf@fed-e.com>
2015-01-24 15:42:38 +0100

    .

--- client/view/xymonfe.js
+++ client/view/xymonfe.js
@@ -1,13 +1,25 @@
 Template.xymonfe.helpers({
   hostname: function(key) {
     retval = key.split("::");
-    $("#vlabel_"+retval[2]).css("background-color", retval[4]);
     return retval[2]
   },
 
+  service: function(key) {
+    var retval = key.split("::");
+    return retval[1];
+  },
+
   info: function(key) {
-    console.log(retval[3], retval[4]);
+    var retval = key.split("::");
+    console.log(retval[3]);
     return retval[3]
+  },
+
+  val: function(key, value) {
+    var retval = value.split("::");
+    console.log(retval[0], retval[1]);
+    $("#vlabel_"+key).css("background-color", retval[0]);
+    return retval[1];
   },
 
   xymon: function() {
